0c0e165d5903f5a07d740e8f4c9fa06c1f9e42ce
dannaliz <marquezdanna@ciencias.unam.mx>
2023-11-07 16:53:43 -0600

    Segmentador 2

--- Segmentador.py
+++ Segmentador.py
@@ -4,15 +4,8 @@ import sys
 import Filtro
 
 def indiceDeCobertura(arreglo):
-    alto,ancho,prof=arreglo.shape
-    total,nubes=0,0
-    arr_nubes=np.array([255,255,255,255])
-    cielo=np.array([0,0,0,255])
-    for i in range(alto):
-        for j in range(ancho):
-            if arreglo[i,j][3]==0:
-                pass
-            if np.array_equal(arreglo[i,j], arr_nubes):
-                nubes+=1
-            total+=1
+    alfa=arreglo[...,3]
+    total=np.sum(alfa>0)
+    nubes=np.sum(np.all(arreglo[:,:,:4]==[255,255,255,255],axis=2))
+    print(total,nubes)
     return float(nubes/total)
