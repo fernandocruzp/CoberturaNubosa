dc041bea2a05f1eeabc4313be0563ee175d168c4
fernandocruzp <112268917+fernandocruzp@users.noreply.github.com>
2023-11-07 20:59:14 -0600

    Manejo de errores filtro

--- main/Filtro.py
+++ main/Filtro.py
@@ -14,21 +14,29 @@ def filtrar(nombre):
     Ejemplo:
         >>> arreglo = filtrar("11838.jpg")
     """
-    imagen = cv2.imread(nombre)
-    ceros = np.array([80, 60, 60], dtype=np.uint8)
-    limite = 0.95
-    mask1 = np.all(imagen < ceros, axis=-1)
-    epsilon = 1e-6
-    rojo = imagen[..., 0] + epsilon
-    mask2 = imagen[..., 2] / rojo < limite
+    try:
+        imagen = cv2.imread(nombre)
+        if imagen is None:
+            print(f"No se pudo abrir la imagen: {nombre}")
+            return None
 
-    rgba_imagen = np.zeros((imagen.shape[0], imagen.shape[1], 4), dtype=np.uint8)
-    rgba_imagen[..., :3] = imagen
-    rgba_imagen[..., 3] = 255
-    rgba_imagen[mask1, 3] = 0
-    rgba_imagen[mask2, :3] = 0
+        ceros = np.array([80, 60, 60], dtype=np.uint8)
+        limite = 0.95
+        mask1 = np.all(imagen < ceros, axis=-1)
+        epsilon = 1e-6
+        rojo = imagen[..., 0] + epsilon
+        mask2 = imagen[..., 2] / rojo < limite
 
-    return rgba_imagen
+        rgba_imagen = np.zeros((imagen.shape[0], imagen.shape[1], 4), dtype=np.uint8)
+        rgba_imagen[..., :3] = imagen
+        rgba_imagen[..., 3] = 255
+        rgba_imagen[mask1, 3] = 0
+        rgba_imagen[mask2, :3] = 0
+
+        return rgba_imagen
+    except Exception as e:
+        print(f"Error al procesar la imagen: {str(e)}")
+        return None
 
 if __name__ == "__main__":
     arreglo = filtrar("11838.jpg")
