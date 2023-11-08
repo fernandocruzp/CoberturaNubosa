cf8cdab240538f44b91f909868c1e8e9f02a17a1
dannaliz <marquezdanna@ciencias.unam.mx>
2023-11-07 20:43:37 -0600

    comentario segmentador 1

--- main/Segmentador.py
+++ main/Segmentador.py
@@ -4,6 +4,15 @@ import sys
 import Filtro
 
 def indiceDeCobertura(arreglo):
+    """
+    Busca un ticket en un archivo CSV y devuelve el clima de la ciudad de origen y destino asociada al ticket.   
+
+    Args: 
+    arreglo: con 3 dimensiones, ancho, alto y rgb 
+
+    Returns:
+        float: cobertura nubosa
+    """
     alfa=arreglo[...,3]
     total=np.sum(alfa>0)
     nubes=np.sum(np.all(arreglo[:,:,:4]==[255,255,255,255],axis=2))
